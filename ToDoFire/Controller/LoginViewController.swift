4ac3201b0da73c431f67d3a9475e52cfa372302c
Юханов Сергей Сергеевич <s.yukhanov@gmail.com>
2018-12-24 15:57:15 +0300

    Added save email in database

--- ToDoFire/Controller/LoginViewController.swift
+++ ToDoFire/Controller/LoginViewController.swift
@@ -66,7 +66,7 @@ class LoginViewController: UIViewController {
             displayWarningLabel(withText: "Info is incorrect")
             return
         }
-        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] (user, error) in
           
          
             
@@ -75,6 +75,9 @@ class LoginViewController: UIViewController {
                 print(error!.localizedDescription)
                 return
             }
+            
+            let userRef = self?.ref.child((user?.user.uid)!)
+            userRef?.setValue(["email": user?.user.email])
             
             
         }
